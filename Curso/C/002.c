6a38f0bd0d69d86644fda2713f9bd61bf96e3bed
Luan Gustavo Altruda Filipov <luan.gustavo.filipov@gmail.com>
2020-09-08 13:19:06 -0300

    New files

---
+++ Curso/C/002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+
+    printf("Ola mundo!");
+
+    return 0;
+
+}
---
+++ Curso/C/003.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+
+    printf("Olá sou o Luan!");
+
+    return 0 ;
+
+}
